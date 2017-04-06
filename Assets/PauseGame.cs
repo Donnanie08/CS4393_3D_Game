bdad426e249f8eec45c9767de1d2628821a08073
Xin Nie <donnanie08@gmail.com>
2017-04-05 23:10:30 -0500

    Added both “P” and “Esc” button activating pause menu

--- Assets/PauseGame.cs
+++ Assets/PauseGame.cs
@@ -20,7 +20,7 @@ public class PauseGame : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Escape)) {
+		if (Input.GetKeyDown (KeyCode.Escape) || Input.GetKeyDown (KeyCode.P)) {
 			OnPause ();
 		}
 	}
