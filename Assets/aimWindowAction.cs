27d02f9a48e6ccfeeb5cc428f44c634395557763
Xin Nie <donnanie08@gmail.com>
2017-04-19 01:14:02 -0500

    Added an aim window

---
+++ Assets/aimWindowAction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class aimWindowAction : MonoBehaviour {
+	public Image AimWindow;
+	// Use this for initialization
+	void Start () {
+		AimWindow.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape) || Input.GetKeyDown (KeyCode.P)) {
+			AimWindow.enabled = false;
+		}
+	}
+
+
+}
